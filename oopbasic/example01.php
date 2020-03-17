c26fb592843f2614b773bc5d7bc12e15aae18174
Lakshya Shah <shahlakshya1988@gmail.com>
2020-03-17 11:17:34 +0530

    5. Bootstrapping An Object

--- oopbasic/example01.php
+++ oopbasic/example01.php
@@ -5,6 +5,14 @@ class Video{
     public $published = false;
     public $title;
 
+    public function __construct(string $type,float $duration,string $title,$published = false){
+        echo PHP_EOL."Called Automatically".PHP_EOL;
+        $this->type = $type;
+        $this->duration = $duration;
+        $this->title = $title;
+        $this->published = $published;
+    }
+
     public function play(){
         return $this->published ? "Video is Playing" : "Video is not Available yet";
 
@@ -12,9 +20,12 @@ class Video{
     public function pause(){
         return $this->published ? "Video Is Playing" : "" ;
     }
+    public function __destruct(){
+        echo PHP_EOL."Object is Getting Destroyed".PHP_EOL;
+    }
 }
 header("Content-Type:text/plain",true);
-$v1 = new Video();
+$v1 = new Video("Educational",03.00,"Video 1",false);
 $v1->author = "Shah Lakshya";
 // echo print_r($v1);
 echo PHP_EOL;
@@ -30,8 +41,7 @@ echo PHP_EOL;
 echo PHP_EOL;
 echo "V2";
 echo PHP_EOL;
-$v2 = new Video();
-$v2->published = true;
+$v2 = new Video("Entertainment",5.30,"Video 2",true);
 echo $v2->play();
 echo PHP_EOL;
 echo $v2->pause();
